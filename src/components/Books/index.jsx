70be5cb157a951aafb74d87c14aca6e148bb9387
Jhonatan Lima <39390551+jhonyllima@users.noreply.github.com>
2020-08-28 18:43:29 -0300

    Create Books Component

--- src/components/Books/index.jsx
+++ src/components/Books/index.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { Container } from './styles';
+import api from '../../Services/api';
+
+import { Container, ImgBook } from './styles';
 
 export default function Books() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+
+    async function listBooks() {
+      try {    
+        const response = await api.get('volumes?q=HARRY%20POTTER');
+        const books = response.data.items;
+        setItems(books);
+      } 
+      catch (error) 
+      {
+        console.log('Error', error.message);
+      }
+    }
+
+    listBooks();
+
+  }, []);
+
   return (
     <Container>
+
+      <ImgBook>
+        {items.map(item => (
+          <img
+            key={item.id}
+            src={item.volumeInfo.imageLinks?.smallThumbnail}
+            alt={item.volumeInfo.imageLinks?.title}
+          />
+        ))}
+      </ImgBook>
 
     </Container>
   );
--- src/components/Books/styles.js
+++ src/components/Books/styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 550px;
-  margin: 40px auto;  
+  max-width: 800px;
+  margin: 20px auto;  
 
   p{
 
@@ -14,5 +14,26 @@ export const Container = styled.div`
       color: orange;
       font-weight: 400;
     }
+`;
+
+export const ImgBook   = styled.div`
+  margin: auto;
+  max-width: 700px;
+
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
   
+
+  img   {
+    width: 100px;
+    height: auto;
+    margin: 10px 10px;
+    border-radius: 4px;
+  }
+
+  p {
+    color: orange;
+    font-size: 0.5rem;
+  }
 `;
